ab8048c49cae3e242510a73ee4ca1298bc1c65f0
Nobos Software <nobosoftware@gmail.com>
2024-04-04 16:30:06 -0400

    Initial Commit

---
+++ Cratedig/CratedigApp.swift
@@ -0,0 +1,17 @@
+//
+//  CratedigApp.swift
+//  Cratedig
+//
+//  Created by Noah Boyers on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CratedigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
